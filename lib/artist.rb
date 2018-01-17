9f8886a5ad87b6a608efa89c39ef8ced189e4261
Petya Djanovska <petya.djanovska@gmail.com>
2018-01-17 22:12:08 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,8 +11,8 @@ class Artist
     song.artist = self
   end
 
-  def add_song_by_name(song.name)
-    song = Song.new(song.name)
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
     song.artist = self
   end
 
